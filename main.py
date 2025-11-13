0fcc6579d610cf5f153ccba0d0c6a8881aebfcb0
edu2ucoc <edu2.ucoc@gmail.com>
2025-11-13 14:40:14 +0900

    Feat : 수정1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def home():
-    return templates.TemplateResponse("index.html") 
+def home(req:Request):
+    return templates.TemplateResponse("index.html",{"request":req}) 
